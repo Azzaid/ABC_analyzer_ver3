ee67c0c40638d18689e039f0cefbc3b63b5a3032
Azzaid <johanas.azzaid@gmail.com>
2017-08-05 23:47:32 +0300

    Almost fully working ABC analysis part. Still need some debug and implementation of grafik method for defining abc groups borders. Still no gui.

--- ABC_autoanalizator_VER3.py
+++ ABC_autoanalizator_VER3.py
@@ -7,10 +7,11 @@ values_for_abc = {'Доход': {'column_name': 'Доход', 'function': 'sum',
                   'Товарооборот': {'column_name': 'Товарооборот', 'function': 'sum', 'abc_groups': [0, 80, 95, 110]},
                   'Количество': {'column_name': 'Количество', 'function': 'count', 'abc_groups': [0, 80, 95, 110]}}
 
-levels_for_abc = [['Группа', 'Подгруппа', 'Товар'], ['Группа', 'Подгруппа'], ['Группа']]
+levels_for_abc = ['Группа', 'Подгруппа', 'Товар']
 
-periods_for_abc = [['06-2015', '10-2015'], ['11-2015', '05-2016'], ['06-2016', '10-2016'], ['11-2016', '05-2017']]
-# how many columns of group sugroup an so on are in excell file counting from 1
+#periods_for_abc = ['06-2015:10-2015', '11-2015:05-2016', '06-2016:10-2016', '11-2016:05-2017']
+periods_for_abc = []
+# how many columns of group subgroup an so on are in excell file counting from 1
 grouping_depth = 2
 
 # choose output excel files
@@ -26,21 +27,28 @@ indexed_sales = data_to_analyse.set_index(['Группа', 'Подгруппа',
 indexed_goods = Goods_table.set_index(['Группа', 'Подгруппа', 'Товар'], drop=True, inplace=False, append=True)
 
 # check whether the names of goods in data_to_analyse are the same as in the Goods_table an get the list of possible name changes
-bad_names_list = Name_checker.check(check_table=indexed_sales, master_table=indexed_goods, recursion_depth=1, grouping_depth=grouping_depth)
+bad_names_list = Name_checker.check(check_table=indexed_sales,
+                                    master_table=indexed_goods,
+                                    recursion_depth=1,
+                                    grouping_depth=grouping_depth)
 print(bad_names_list)
 
 # list all months in sales table
 data_to_analyse = data_to_analyse.sort_index()
-begin_date = data_to_analyse.head(1).index[0]                                                                                #
+begin_date = data_to_analyse.head(1).index[0]
 end_date = data_to_analyse.tail(1).index[0]
 months_to_analyse = pandas.date_range(begin_date, end_date, freq='M', closed='right').strftime('%Y-%m')
 
-list_of_all_times = periods_for_abc.append(months_to_analyse)
-
-all_tables_list = {}
+periods_for_abc.extend(months_to_analyse.tolist())
 
 # abc analysis
-for time_shard in list_of_all_times:
-    all_tables_list[time_shard] = analyze_ABC.deep_analyze()
+for period in periods_for_abc:
+    debug_tables_list, short_tables_list = analyze_ABC.deep_analyze(table=data_to_analyse[period],
+                                               upgroup='main',
+                                               time=period,
+                                               levels_for_ABC=levels_for_abc,
+                                               values_for_ABC=values_for_abc,
+                                               grouping_depth=grouping_depth,
+                                               recursion_depth=1)
 
 # constructing output table
--- analyze_ABC.py
+++ analyze_ABC.py
@@ -1,6 +1,6 @@
-def analyze(table, pivot_by, value, function, abc_groups, **kwargs):
+import pandas
 
-    import pandas
+def analyze(table, pivot_by, value, function, abc_groups, **kwargs):
 
     Grouped_table = table.pivot_table(index=pivot_by, values=value, aggfunc=function)
 
@@ -14,4 +14,53 @@ def analyze(table, pivot_by, value, function, abc_groups, **kwargs):
 
     return Grouped_table
 
-def deep_analyze()
+
+def deep_analyze(table, time, upgroup, levels_for_ABC, values_for_ABC, grouping_depth, recursion_depth, **kwargs):
+
+    #create dict levels and keys to store analysis results
+    debug_table_list = {levels_for_ABC[recursion_depth]:{upgroup:{}}}
+    short_table_list = {levels_for_ABC[recursion_depth]:{upgroup:{}}}
+
+    #pre-made short table to fill it with info from different values analysis
+    short_table_list[levels_for_ABC[recursion_depth]] = pandas.DataFrame(index=table[levels_for_ABC[recursion_depth]].drop_duplicates(keep='first'),
+                                                                         columns=[[time], ['% дохода']])
+    short_table_list[levels_for_ABC[recursion_depth]].loc[:, (time, 'группы')] = ''
+
+    #calculate income percent and fill it into short table
+    income_table = table.pivot_table(index=levels_for_ABC[recursion_depth],
+                                     values=['Себестоимость', 'Доход'],
+                                     aggfunc='sum')
+    short_table_list[levels_for_ABC[recursion_depth]].loc[:, (time, '% дохода')] = income_table['Доход'] / income_table['Себестоимость'] * 100
+
+    #analyse abc for every value listed in main programm block
+    for value in values_for_ABC:
+        debug_table_list[levels_for_ABC[recursion_depth]][value] = analyze(table = table,
+                                                                                 pivot_by = levels_for_ABC[recursion_depth],
+                                                                                 value = values_for_ABC[value]['column_name'] ,
+                                                                                 function = values_for_ABC[value]['function'],
+                                                                                 abc_groups = values_for_ABC[value]['abc_groups'])
+
+        #fill info about abc group and income percent into short table
+        short_table_list[levels_for_ABC[recursion_depth]].loc[:, (time, 'группы')] += debug_table_list[levels_for_ABC[recursion_depth]][value]['Группа по ' + value].astype(str)
+
+    #analyse subgrupp if there are subgrupp
+    if recursion_depth < grouping_depth:
+
+        #turn group name into index to chose rows for in-group analysis
+        table.set_index([levels_for_ABC[recursion_depth]], drop=True, inplace=True, append=False)
+
+        # analyse subgroups in every group
+        for GroupToDive in table.index:
+            deeper_debug_table_list, deeper_short_table_list = deep_analyze(table=table.xs(GroupToDive, axis=0),
+                                                                            upgroup=GroupToDive,
+                                                                            time=time,
+                                                                            levels_for_ABC=levels_for_ABC,
+                                                                            values_for_ABC=values_for_ABC,
+                                                                            grouping_depth=grouping_depth,
+                                                                            recursion_depth=recursion_depth+1)
+            #add results to a listst
+            debug_table_list.update(deeper_debug_table_list)
+            short_table_list.update(deeper_short_table_list)
+
+    return(debug_table_list, short_table_list)
+
